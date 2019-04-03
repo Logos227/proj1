91657b4added4030e42f702e4343d574f32cde3f
Jiajia  Yang <jyang7@uon.edu.au>
2019-04-03 07:59:47 +0000

    Modified the code for task1

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include < stdio.h >
-#include < string.h >
+#include <stdio.h>
+#include <string.h>
 
 
 int main(){
     char passwd[100], encrypted[100];
-    int i,j,k,t,move;
+    int i, j, k, t, move;
     while(1){
         printf("Enter message to be encrypted:");
         gets(passwd);
@@ -14,7 +14,7 @@ int main(){
             
             if (passwd[i]>='A' && passwd[i]<='Z'){
                 
-                passwd[i] = ((passws[i]-'A')+move)%26+ 'A';  
+                passwd[i] = ((passwd[i]-'A')+move)%26+ 'A';  
             }
             else if (passwd[i]>='a' && passwd[i]<='z'){
                 
