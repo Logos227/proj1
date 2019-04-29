6bf3f1d39edff9fea5f397b9d7ced1cbae6c7a00
Logos227 <48046236+Logos227@users.noreply.github.com>
2019-04-29 15:39:02 +1000

    Final Assignment 1
    Added some comments

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* 
-Jiajia Yang
-C3314189
+Student Name : Jiajia Yang
+Student Number : C3314189
 Engg1003 Assignment 1 
 */
 
@@ -8,73 +8,78 @@ Engg1003 Assignment 1
 #include <string.h>
 #include <stdlib.h>
 
-int CaesarEn( char *message, char *output, int rotationAmount);//caesar cipher encryption function prototype
-int CaesarDe( char *message, char *output, int rotationAmount);//caesar cipher decryption function prototype
-int SubEn( char *message, char *output, const char *key); //substitution Encryption function prototype
-int SubDe( char *message, char *output, const char *key); //substitution Decryption function prototype
+//Function prototypes
+int CaesarEn( char *message, char *output, int rotationAmount);//Caesar cipher encryption function prototype
+int CaesarDe( char *message, char *output, int rotationAmount);//Caesar cipher decryption function prototype
+int SubEn( char *message, char *output, const char *key); //Substitution Encryption function prototype
+int SubDe( char *message, char *output, const char *key); //Substitution Decryption function prototype
 
 int main(){
+    //Text read from a file
     FILE *inputCode, *outputCode;
     inputCode = fopen("inputCode.txt", "r");
     outputCode = fopen("outputCode.txt", "w");
-    if (inputCode == NULL)
+    while (inputCode == NULL)  //Determine if the file exists
     perror("File Input");
-    int i,j,k,choice;
-    float rotationAmount;
-    char message[1024],output[1024],key[30];
+    
+    int i,k,choice;
+    int rotationAmount;
+    char message[100],output[100],key[30];
     char *point;
     rotationAmount = strtol(key, &point, 10);
     
-    
-    //make a selection menu for console 
-    printf("Please make a selection\n");
-    printf("1. Encryption with a rotation cipher key:\n");
-    printf("2. Decryption with a rotation cipher key:\n");
-    printf("3. Encryption with a substitution cipher given plain text and key:\n");
-    printf("4. Decryption with a substitution cipher given plain text and key:\n");
+    //Make a selection menu
+    printf("\nPlease make a selection from 1 to 4\n");
+    printf("1. Encryption with a rotation cipher key\n");
+    printf("2. Decryption with a rotation cipher key\n");
+    printf("3. Encryption with a substitution cipher key\n");
+    printf("4. Decryption with a substitution cipher key\n");
     scanf ("%c",&choice);
-    while ((getchar()) != '\n');
+    if (choice != 1 && choice != 2 && choice != 3 && choice != 4) {//Make sure the entered is not blank 
+    printf("Please select 1 , 2 , 3, \n");
+    scanf("%d", &choice);
+    //while ((getchar()) != '\n');//Make sure the entered is not blank 
     switch(choice){
-        case 1:
-            printf("You have chosen rotation cipher encryption\n");
-            printf("Enter a key (integer between [0,25])\n");
+        case 1://Encryption of a message with a rotation cipher
+            printf("You have chosen rotation cipher encryption.\n");
+            printf("Enter a shift amount (integer between [0,25])\n");
             scanf("%d", &rotationAmount);
             CaesarEn(message, output, rotationAmount);
             printf("%c", output);
             printf("\n");
 
-        case 2:
-            printf("You have chosen rotation cipher decryption\n");
-            printf("1. Decipher with key\n");
-            printf("2. Decipher without key\n");
+        case 2://Decryption of a message encrypted with a rotation cipher
+            printf("You have chosen rotation cipher decryption.\n");
+            printf("Please make a selection between 1 and 2\n");
+            printf("1. Decipher with shift amount\n");
+            printf("2. Decipher without shift amount\n");
             scanf("%d", &choice);
-            if (choice != 1 && choice != 2) {
-               printf("\nPlease select 1 or 2: \n");
+            if (choice != 1 && choice != 2) {//Make sure the entered is not blank 
+               printf("Please select 1 or 2 \n");
                scanf("%d", &choice);
             }
-            if (choice == 1) { 
-               printf("Key: %d\n", rotationAmount);
+            while (choice == 1) { //Decipher with shift amount 
+               printf("Shift amount is : %d\n", rotationAmount);
                CaesarDe(message, output, rotationAmount);
                break;
                 
             }
-            if (choice == 2) { 
+            while (choice == 2) { //Decipher without shift amount 
                rotationAmount = 0;
                CaesarDe(message, output, rotationAmount);
-               
                while (strstr(output, " A ") == NULL && strstr(output, " THE ") == NULL) {
                      rotationAmount++;
                      CaesarDe(message, output, rotationAmount);
                 }
-                printf("Key: %d\n", rotationAmount);
+                printf("Shift amount is : %d\n", rotationAmount);
                 printf("\n");
-                printf("Message Text: \n");
+                printf("Message is : \n");
                 for (i = 0; message[i] != '\0'; i++) {
                     printf("%c", message[i]);
                     
                 }
                 printf("\n");
-                printf("Output Text: \n");
+                printf("Output is : \n");
                 for (i = 0; message[i] != '\0'; i++) {
                     printf("%c", output[i]);
                     fprintf(output, "%c", output[i]);
@@ -84,38 +89,40 @@ int main(){
                 
             }
            
-         case 3:
+         case 3://Encryption of a message with a substitution cipher
+         //Return value is "k"
          k = SubEn(message, output, key);
-         if (k == 1)
+         while (k == 1)
             exit(k);
-         printf("\nKey: %s\n\n", key);
-         printf("Message Text: \n");
+         printf("\nKey: %s\n", key);
+         printf("Message is : \n");
          for (i = 0; message[i] != '\0'; i++) {
              printf("%c", message[i]);
              
          }
          printf("\n");
-         printf("Output Text: \n");
+         printf("Output is : \n");
          for (i = 0; message[i] != '\0'; i++) {
              printf("%c", output[i]);
              fprintf(output, "%c", output[i]);
              
          }
          printf("\n");
-      case 4:
          
+      case 4: //Decryption of a message encrypted with a substitution cipher
+         //Return value is "k"
          k = SubDe(message, output, *key);
-         if (k == 1)
+         while (k == 1)
             exit(k);
          printf("\nKey: %s\n", key);
          printf("\nKey: %s\n", key);
-         printf("Message Text: \n");
+         printf("Message is: \n");
          for (i = 0; message[i] != '\0'; i++) {
              printf("%c", message[i]);
              
          }
          printf("\n");
-         printf("Output Text: \n");
+         printf("Output is: \n");
          for (i = 0; message[i] != '\0'; i++) {
              printf("%c", output[i]);
              fprintf(output, "%c", output[i]);
@@ -127,90 +134,94 @@ int main(){
    fclose(stdin);
    return k;
 }
+}
     
-    int CaesarEn( char *message,char *output, int rotationAmount){
+//Some function definetions 
+//Caesar cipher encryption function definetion
+int CaesarEn( char *message,char *output, int rotationAmount){
     int i;
     for (i = 0; i < strlen (message); i++){
-        if (message[i]>='A' && message[i]<='Z'){
-            //Encryt the UPPER CASE letters 
-            message[i] = ((message[i]-'A')+rotationAmount)%26+ 'A';
-            
+        if (message[i] >= 'A' && message[i] <= 'Z'){
+            //Encryption of the UPPER CASE letters 
+            message[i] = ((message[i] - 'A') + rotationAmount) %26 + 'A';
         }
-        else if (message[i]>='a' && message[i]<='z'){ 
+        else if (message[i] >= 'a' && message[i] <= 'z'){ 
             //Find the lower case letters and change it to UPPER CASE letters
             message[i] = message[i]-32; 
-            message[i] = ((message[i]-'A')+rotationAmount)%26+ 'A';
+            message[i] = ((message[i] - 'A') + rotationAmount) %26 + 'A';
         }    
     }
     return 0;
-        
-    }
     
-    
-    int CaesarDe(char *message, char *output, int rotationAmount){
-        int i;
-        for (i = 0; i < strlen (message); i++){
-            
-            if (message[i]>='A' && message[i]<='Z'){
-                
-                message[i] = ((message[i]-'A')-rotationAmount)%26+ 'A';  
-            }
-            else if (message[i]>='a' && message[i]<='z'){
-                message[i] = message[i]-32;
-                message[i] = ((message[i]-'A')-rotationAmount)%26+ 'A';
-            }    
-        }
-        return 0;
-    }
-        
-
-     int SubEn( char *message, char *output, const char *key){
- 
-        int i, j[1024];
-        for ( i = 0; message[i] != '\0'; i++){
-            if(message[i]>='a' && message[i]<='z'){
-              j[i] = message[i] - 65;
-              output[i] = key[j[i]];
-            }
-            else
-            output[i] = message[i];
-            
-        }
-        return 0;
-         
-     }
-             
-    int SubDe( char *message, char *output, const char *key){
-        const char code[26]={"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-        char OriginalCode;
-        char NewestCode;
-        int i,m,n=0;
-        for (i = 0; message[i] != '\0'; i++){
-            OriginalCode = key[i];
-            NewestCode = code[i];
-            if(message[i]>='A' && message[i]<='Z'){
-              for (m = 0; m < 1025; m++){
-            if (message[i] == OriginalCode){
-               output[i] = NewestCode;
-               break;
-                
-            }
-            else{
-               m++;
-               OriginalCode = key[n];
-               NewestCode = code[n];
-               n++;
-               if (n > 25)
-                  n = 0;
-                
-            }
-                  
-              }
-                
-            }
-            else
-         output[i] = message[i];
-   }
-   return 0;
 }
-             
+    
+//Caesar cipher decryption function definetion
+int CaesarDe(char *message, char *output, int rotationAmount){
+    int i;
+    for (i = 0; i < strlen (message); i++){
+            
+        if (message[i] >= 'A' && message[i] <= 'Z'){
+                
+            message[i] = ((message[i] - 'A') - rotationAmount) %26 + 'A';  
+        }
+        else if (message[i] >= 'a' && message[i] <= 'z'){
+            message[i] = message[i] - 32;
+            message[i] = ((message[i] - 'A') - rotationAmount) %26 + 'A';
+        }    
+    }
+    return 0;
+    
+}
+        
+//Substitution Encryption function definetion
+int SubEn( char *message, char *output, const char *key){
+    int i, j[1024];
+    for ( i = 0; message[i] != '\0'; i++){
+    if(message[i]>='a' && message[i]<='z'){
+        j[i] = message[i] - 65;
+        output[i] = key[j[i]];
+        
+    }
+    else
+    output[i] = message[i];
+        
+    }
+    return 0;
+    
+}
+
+//Substitution Decryption function definetion         
+int SubDe( char *message, char *output, const char *key){
+    const char code[26]={"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    char OriginalCode;
+    char NewestCode;
+    int i,m,n=0;
+    for (i = 0; message[i] != '\0'; i++){
+        OriginalCode = key[i];
+        NewestCode = code[i];
+        if(message[i]>='A' && message[i]<='Z'){
+            for (m = 0; m < 1025; m++){
+                if (message[i] == OriginalCode){
+                    output[i] = NewestCode;
+                    break;
+                    
+                }
+                else{
+                    m++;
+                    OriginalCode = key[n];
+                    NewestCode = code[n];
+                    n++;
+                    if (n > 25)
+                    n = 0;
+                    
+                }
+                
+            }
+            
+        }
+        else
+        output[i] = message[i];
+        
+    }
+    return 0;
+}
